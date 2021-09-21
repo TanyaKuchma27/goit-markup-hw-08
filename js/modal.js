111636079d6ae307cc3c1f1c38df14209eeb4411
Tanya Kuchma <kuchma.tanya@ukr.net>
2021-09-21 18:02:56 +0300

    try to do hw without img

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,7 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    // document.body.classList.tooggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
   }
 })();
